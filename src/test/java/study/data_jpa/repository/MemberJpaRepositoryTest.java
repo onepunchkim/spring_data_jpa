08b6c326662ec0322f7a5ec1f3c430bf7eeffa13
onepunchkim <hyukjin4711@gmail.com>
2026-03-08 19:32:12 +0900

    start

--- src/test/java/study/data_jpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/data_jpa/repository/MemberJpaRepositoryTest.java
@@ -1,6 +1,5 @@
 package study.data_jpa.repository;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,7 +8,6 @@ import org.springframework.transaction.annotation.Transactional;
 import study.data_jpa.entity.Member;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
