90b54bf74d09a7f490267c8d5b8591ba5e5410c4
성스러운 성진이 <hyukjin4711@gmail.com>
2026-03-20 11:27:10 +0900

    JPA bulk update

--- src/main/java/study/data_jpa/repository/MemberRepository.java
+++ src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.data_jpa.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import study.data_jpa.entity.Member;
@@ -40,4 +41,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query(value = "select m from Member m",
     countQuery = "select count(m.username) from Member m")
     Page<Member> findByAge(int age, PageRequest pageable);
+
+    @Modifying
+    @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
+    int bulkAgePlus(@Param("age") int age);
 }
