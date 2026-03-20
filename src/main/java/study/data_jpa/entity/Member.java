15351b3bd06d78d21bce5ca851f01d4bb4269bac
onepunchkim <hyukjin4711@gmail.com>
2026-03-20 18:20:35 +0900

    SpringData JPA Auditing

--- src/main/java/study/data_jpa/entity/Member.java
+++ src/main/java/study/data_jpa/entity/Member.java
@@ -13,7 +13,7 @@ import lombok.*;
 )
 @NamedEntityGraph(name = "Member.all", attributeNodes =
 @NamedAttributeNode("team"))
-public class Member extends JpaBaseEntity {
+public class Member extends BaseEntity {
 
     @Id @GeneratedValue
     @Column(name = "member_id")
